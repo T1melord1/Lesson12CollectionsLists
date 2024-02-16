c3db84b0e7d4f85437634880ff6e3d357b0ab93c
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-16 16:44:28 +0300

    Создание от интерфейса List

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList();
-        for (int i = 0; i < 100 ; i++) {
-            list.add(i);
-        }
-        list.forEach(System.out::println);
+        List<Integer> list = new ArrayList();
+
         }
     }
     
