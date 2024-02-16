51c9bdabf10410a7f4d4e69e5d47cf4a89aeafe8
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-16 16:53:32 +0300

    В листах вместо метода length() как в массивах используется size()

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static List<String> todos = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList();
-тест
+    }
+
+        public static void add(String todo){
+        if(todos.contains(todo)) {
+            System.out.println("Дело " + todo + " уже есть в списке!");
+            return;
+        }
+
+        todos.add(todo);
+            System.out.println("Дело " + todo + " успешно добавлено");
+    }
+    public static void delete(Integer index){
+        if(todos.size() <= index){
+            System.out.println("Нет такого индекса!");
+            return;
+        }
+        String todo = todos.get(index);
+        todos.remove(index);
+        System.out.println("Дело " + todo + " успешно удалено!");
+//        add if not exist
+//        delete if exist
+//        print all
+//        print by index
+
         }
     }
     
